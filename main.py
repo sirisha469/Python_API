e40dac499ea0c1d591a01d98a89126722fbd41b0
sirisha469 <chsiri469@gmail.com>
2023-09-07 23:08:09 +0530

    post

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
+from pydantic import BaseModel, PostgresDsn
 
 app = FastAPI()
+
+# request get method url: "/"
+
+class Post(BaseModel):
+  title: str
+  content: str
 
 @app.get("/")
 def root():
@@ -9,3 +16,17 @@ def root():
 @app.get("/posts")
 def get_posts():
   return {"data": "This is your post"}
+
+# @app.post("/createposts")
+# def create_posts(payLoad: dict = Body(...)):
+#   #print(payLoad)
+#   return {
+#     "message": "successfully create post", 
+#     "new_post": f"title {payLoad['title']} content {payLoad['content']}"
+#   }
+
+@app.post("/createposts")
+def create_posts(new_post: Post):
+  print(new_post)
+  return {"data": "new post"}
+
