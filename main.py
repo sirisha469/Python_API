15329cb6cc7885eb71d3e4d9043d32dca2a070e7
sirisha469 <chsiri469@gmail.com>
2023-09-08 16:42:12 +0530

    CRD-operations

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Optional
-from fastapi import Body, FastAPI
-from pydantic import BaseModel, PostgresDsn
+from fastapi import Body, FastAPI, Response, status, HTTPException
+from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
 
@@ -12,13 +13,36 @@ class Post(BaseModel):
   published: bool = True
   rating: Optional[int] = None
 
+
+my_posts = [
+  {"title": "title of post 1", "content": "content of post 1", "id":1},
+  {"title": "favorite food", "content": "I like papidi", "id":2}
+]
+
+
+#getting specified ID
+def find_post(id):
+  for p in my_posts:
+    if p['id'] == id:
+      return p
+    
+
+#getting specified index
+def find_index_post(id):
+  for index, p in enumerate(my_posts):
+    if p['id'] == id:
+      return index
+    
+
+
 @app.get("/")
 def root():
   return {"message": "Hello World!!!"}
 
+#read: getting all posts
 @app.get("/posts")
 def get_posts():
-  return {"data": "This is your post"}
+  return {"data": my_posts}
 
 # @app.post("/createposts")
 # def create_posts(payLoad: dict = Body(...)):
@@ -28,10 +52,61 @@ def get_posts():
 #     "new_post": f"title {payLoad['title']} content {payLoad['content']}"
 #   }
 
-@app.post("/posts")
+#create: creating posts 
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
-  print(post.rating)
-  print(post)
-  print(post.model_dump())
-  return {"data": post}
+  # print(post.rating)
+  # print(post)
+  # print(post.model_dump())
+  #model_dump is a dictionary
 
+
+  #id is not there in Post class so we want to create 
+  post_dict = post.model_dump()
+  post_dict['id'] = randrange(0, 100000)
+  my_posts.append(post_dict)
+  return {"data": post_dict}
+
+
+@app.get("/posts/latest")
+def get_latest_post():
+  post = my_posts[len(my_posts)-1]
+  return {"details": post}
+
+
+
+#read: getting single post based on id
+@app.get("/posts/{id}")
+def get_post(id: int, response: Response): 
+  #id: int it default takes the id as integer no need of explicit conversion
+  print(id)
+  print(type(id)) 
+  #here id is a string but our method wants a integer so convert that string into integer
+  #for this we are creating a method(find_post())
+  #post = find_post(int(id))
+
+  post = find_post(id)
+  print(post)
+  if not post:
+    raise HTTPException(status_code= status.HTTP_404_NOT_FOUND,
+                        detail=f"post with id: {id} was not found")
+  
+    # the above code is used to replace the below 2 lines by using this we can also remove the parameter of the method response: Response
+    # response.status_code = status.HTTP_404_NOT_FOUND
+    # return {"message": f"post with id: {id} was not found"}
+  return {"single data": post }
+
+
+#delete: deleting post
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+  #find the index in the array that has required ID
+  #my_posts.pop(index)
+
+  index = find_index_post(id)
+
+  if index == None:
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = f"post with id: {id} does not exist")
+  my_posts.pop(index)
+  #return {"message": "post successfully deleted"}
+  return Response(status_code=status.HTTP_204_NO_CONTENT)
