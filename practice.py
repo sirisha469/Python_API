24faddd74136665b4b4a0d2006038761fd9b7dc6
sirisha469 <chsiri469@gmail.com>
2023-09-14 12:30:45 +0530

    update-delete

--- practice.py
+++ practice.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI, Response, status, HTTPException
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from pydantic import BaseModel
@@ -42,3 +42,47 @@ def get_students():
   cursor.execute(""" SELECT * FROM students""")
   students = cursor.fetchall()
   return {"students": students}
+
+
+#getting student details by id
+@app.get("/students/{id}")
+def get_student(id: str):
+  cursor.execute(""" SELECT * FROM students WHERE id = %s """,(id))
+  student = cursor.fetchone()
+
+  print(student)
+  if not student:
+    raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} not found")
+  
+  return {"student": student} 
+
+
+#deleting student
+@app.delete("/students/{id}")
+def delete_student(id: str):
+  cursor.execute(""" DELETE from students WHERE id = %s returning *""", (id))
+  deleted_student = cursor.fetchone()
+
+  conn.commit()
+
+  if deleted_student == None:
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"post with id: {id} was not found")
+
+  #return {"daleted student": deleted_student}
+  return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+
+@app.put("/students/{id}")
+def update_student(id: str, stu: student):
+  cursor.execute(""" update students set name=%s, branch=%s, mobile=%s where id=%s returning * """, (stu.name, stu.branch, stu.mobile, id))
+  updated_student = cursor.fetchone()
+
+  print(updated_student)
+  conn.commit()
+
+  if updated_student == None:
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = f"post with id: {id} does not exist")
+
+  return {"updated student":updated_student}
+  
