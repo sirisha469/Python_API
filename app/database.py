96dcf6f022f3fb9a624bf99e622978a90b777289
sirisha469 <chsiri469@gmail.com>
2023-09-16 10:53:53 +0530

    orm-db-created

--- app/database.py
+++ app/database.py
@@ -3,10 +3,19 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postrges:0000@localhost/py_fastapi'
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:0000@localhost/py_fastapi'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autoflush=False, autocommit= False, bind = engine)
 
 Base = declarative_base()
+
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- app/main.py
+++ app/main.py
@@ -6,21 +6,12 @@ from psycopg2.extras import RealDictCursor
 import time
 
 from . import models
-from .database import engine, SessionLocal
+from .database import engine, get_db
 from sqlalchemy.orm import Session
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-# Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
 
 
 class Post(BaseModel):
