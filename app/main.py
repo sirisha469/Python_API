c5a264d74ce6b52acf8d43981b5305303d14a76e
sirisha469 <chsiri469@gmail.com>
2023-09-29 16:13:43 +0530

    change-models-user-table-reflects-alembic

--- app/main.py
+++ app/main.py
@@ -14,3 +14,8 @@ app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(authentication.router)
 app.include_router(vote.router)
+
+
+@app.get("/")
+def root():
+  return {"message":"Hello world"}
