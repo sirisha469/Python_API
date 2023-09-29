cec7108c82eacaaf376f01b43b89e4b24a671a3a
sirisha469 <chsiri469@gmail.com>
2023-09-29 16:50:01 +0530

    domains-setting

--- app/main.py
+++ app/main.py
@@ -4,10 +4,20 @@ from .database import engine, get_db
 from .routers import post, user, authentication, vote
 from .config import settings
 
+from fastapi.middleware.cors import CORSMiddleware
 
-models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(post.router)
